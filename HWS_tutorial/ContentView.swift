df7d73981d60316c6d64c08af3df8e91cef64140
Vaibhav Upadhyay <uvaibhavb@gmail.com>
2024-08-25 19:36:03 +0530

    We split Swift demo

---
+++ HWS_tutorial/ContentView.swift
@@ -0,0 +1,73 @@
+//
+//  ContentView.swift
+//  HWS_tutorial
+//
+//  Created by वैभव उपाध्याय on 25/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFoucsed: Bool
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSeletcion = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSeletcion
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        return amountPerPerson
+    }
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "INR"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFoucsed)
+                    
+                    
+                    Picker("Number of peopel", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
+                
+                Section("How much do you want to tip?") {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                if amountIsFoucsed {
+                    Button("Done") {
+                        amountIsFoucsed = false
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
+    
---
+++ HWS_tutorial/HWS_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWS_tutorialApp.swift
+//  HWS_tutorial
+//
+//  Created by वैभव उपाध्याय on 25/08/24.
+//
+
+import SwiftUI
+
+@main
+struct HWS_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
